0851b1ce2dd814c95663efb6dc67e20cc1576455
nicostav <nicostav@protonmail.com>
2025-02-23 11:48:04 +0100

    feature(install): added checks to install script
    - Checks if run with root privileges
    - Checks if service user already exists
    - Checks before creating directories
    - Checks before copying files

--- install.sh
+++ install.sh
@@ -1,22 +1,90 @@
 #!/bin/bash
 
+# Check if run with root rights
+if [ "$EUID" -ne 0 ]
+  then echo "[!] Please run as root"
+  exit
+fi
+
 # Create service user / System User - No Home - No Login
-sudo useradd -r -M -s /usr/sbin/nologin bitnodemetrics
+user_exists(){ id -u "bitnodemetrics" &>/dev/null; } 
+if user_exists "$1"; code=$?; then 
+    echo 'User bitnodemetrics already exists'
+else
+    sudo useradd -r -M -s /usr/sbin/nologin bitnodemetrics
+    echo "[*] User bitnodemetrics created"
+fi
 
 # Create etc directories needed
-sudo mkdir /etc/bitnodeui
-sudo mkdir /etc/bitnodeuimetrics
+if [ -d "/etc/bitnodeui" ]; then
+    echo "[?] Directory /etc/bitnodeui/ already exists"
+else
+    sudo mkdir /etc/bitnodeui
+    echo "[*] Directory /etc/bitnodeui/ created"
+fi
+
+if [ -d "/etc/bitnodeui/metrics" ]; then
+    echo "[?] Directory /etc/bitnodeui/metrics already exists"
+else
+    sudo mkdir /etc/bitnodeui/metrics
+    echo "[*] Directory /etc/bitnodeui/metrics created"
+fi
 sudo chown bitnodemetrics:bitnodemetrics /etc/bitnodeui/metrics
 
 # Copy python
-sudo cp source/metrics-service.py /etc/bitnodeui/metrics/
-sudo chown bitnodemetrics:bitnodemetrics /etc/bitnodeui/metrics/metrics-service.py
-sudo chmod +x /etc/bitnodeui/metrics/metrics-service.py
-sudo touch /etc/bitnodeui/metrics/metrics.db
-sudo chown bitnodemetrics:bitnodemetrics /etc/bitnodeui/metrics/metrics.db
+if [ -f "source/metrics-service.py" ]; then
+    if [ -f "/etc/bitnodeui/metrics/metrics-service.py" ]; then
+        echo "[?] File /etc/bitnodeui/metrics/metrics-service.py already exists"
+    else
+        sudo cp source/metrics-service.py /etc/bitnodeui/metrics/
+        sudo chown bitnodemetrics:bitnodemetrics /etc/bitnodeui/metrics/metrics-service.py
+        sudo chmod +x /etc/bitnodeui/metrics/metrics-service.py
+        echo "[*] File /etc/bitnodeui/metrics/metrics-service.py created"
+    fi
+else
+    echo "[!] Source file for metrics-service.py does not exist"
+    exit
+fi
+
+if [ -f "source/metrics.db" ]; then
+    if [ -f "/etc/bitnodeui/metrics/metrics.db" ]; then
+        echo "[?] File /etc/bitnodeui/metrics/metrics.db already exists"
+    else
+        sudo cp source/metrics.db /etc/bitnodeui/metrics/
+        sudo chown bitnodemetrics:bitnodemetrics /etc/bitnodeui/metrics/metrics.db
+        echo "[*] File /etc/bitnodeui/metrics/metrics.db created"
+    fi
+else
+    echo "[!] Source file for metrics.db does not exist"
+    exit
+fi
 
 # Configure Service
-sudo cp source/bitnode-metrics.service source/bitnode-metrics.timer /etc/systemd/system/
+if [ -f "source/bitnode-metrics.service" ]; then
+    if [ -f "/etc/systemd/system/bitnode-metrics.service" ]; then
+        echo "[?] File /etc/systemd/system/bitnode-metrics.service already exists"
+    else
+        sudo cp source/bitnode-metrics.service /etc/systemd/system
+        echo "[*] File /etc/systemd/system/bitnode-metrics.service created"
+    fi
+else
+    echo "[!] Source file for bitnode-metrics.service does not exist"
+    exit
+fi
+
+if [ -f "source/bitnode-metrics.timere" ]; then
+    if [ -f "/etc/systemd/system/bitnode-metrics.timer" ]; then
+        echo "[?] File /etc/systemd/system/bitnode-metrics.timer already exists"
+    else
+        sudo cp source/bitnode-metrics.timer /etc/systemd/system
+        echo "[*] File /etc/systemd/system/bitnode-metrics.timer created"
+    fi
+else
+    echo "[!] Source file for bitnode-metrics.timer does not exist"
+    exit
+fi
+
+# Reload and enable/start service
 sudo systemctl daemon-reload
 sudo systemctl enable bitnode-metrics.timer
 sudo systemctl start bitnode-metrics.timer
